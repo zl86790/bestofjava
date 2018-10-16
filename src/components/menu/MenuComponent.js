2deb8731cca52d960a0c606f29714e1edd3baa30
pactera-admindev-lizhe <zhe.li4@pactera.com>
2018-10-16 15:33:46 +0800

    order the menu

--- src/components/menu/MenuComponent.js
+++ src/components/menu/MenuComponent.js
@@ -6,9 +6,14 @@ const MenuComponent = props => {
   return (
     <div className="bojLeftMenu">
       <MenuCard label="Home" link="/" />
-      <MenuCard label="Spring" link="/topic" topic="spring" />
-      <MenuCard label="Maven" link="/topic" topic="maven" />
+      <MenuCard label="Activiti" link="/topic" topic="activiti" />
+      <MenuCard label="Lucene" link="/topic" topic="lucene" />
       <MenuCard label="Mybatis" link="/topic" topic="mybatis" />
+      <MenuCard label="Maven" link="/topic" topic="maven" />
+      <MenuCard label="Rabbitmq" link="/topic" topic="rabbitmq" />
+      <MenuCard label="Redis" link="/topic" topic="redis" />
+      <MenuCard label="Rest" link="/topic" topic="rest" />
+      <MenuCard label="Spring" link="/topic" topic="spring" />
     </div>
   );
 };
