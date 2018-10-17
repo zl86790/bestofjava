1140f91876d1de985103de156b37cf528b53985b
pactera-admindev-lizhe <zhe.li4@pactera.com>
2018-10-17 14:00:32 +0800

    refactor menu

--- src/components/menu/MenuComponent.js
+++ src/components/menu/MenuComponent.js
@@ -5,15 +5,17 @@ import MenuCard from "./menuCard/MenuCardComponent";
 const MenuComponent = props => {
   return (
     <div className="bojLeftMenu">
-      <MenuCard label="Home" link="/" />
-      <MenuCard label="Activiti" link="/topic" topic="activiti" />
-      <MenuCard label="Lucene" link="/topic" topic="lucene" />
-      <MenuCard label="Mybatis" link="/topic" topic="mybatis" />
-      <MenuCard label="Maven" link="/topic" topic="maven" />
-      <MenuCard label="Rabbitmq" link="/topic" topic="rabbitmq" />
-      <MenuCard label="Redis" link="/topic" topic="redis" />
-      <MenuCard label="Rest" link="/topic" topic="rest" />
-      <MenuCard label="Spring" link="/topic" topic="spring" />
+      <nav>
+        <MenuCard label="Home" link="/" />
+        <MenuCard label="Activiti" link="/topic/activiti" />
+        <MenuCard label="Lucene" link="/topic/lucene" />
+        <MenuCard label="Mybatis" link="/topic/mybatis" />
+        <MenuCard label="Maven" link="/topic/maven" />
+        <MenuCard label="Rabbitmq" link="/topic/rabbitmq" />
+        <MenuCard label="Redis" link="/topic/redis" />
+        <MenuCard label="Rest" link="/topic/rest" />
+        <MenuCard label="Spring" link="/topic/spring" />
+      </nav>
     </div>
   );
 };
--- src/components/menu/menuCard/MenuCardComponent.js
+++ src/components/menu/menuCard/MenuCardComponent.js
@@ -3,22 +3,16 @@ import "./css/menuCardComponentStyle.css";
 import { Link } from "react-router-dom";
 
 const MenuCardComponent = props => {
-  if (props.topic) {
-    return (
-      <div className="menuCard">
-        <Link to={props.link + "/" + props.topic} activeClassName="active">
-          {props.label}
-        </Link>
-      </div>
-    );
-  } else {
-    return (
-      <div className="menuCard">
-        <Link to={props.link} activeClassName="active">
-          {props.label}
-        </Link>
-      </div>
-    );
-  }
+  return (
+    <div className="menuCard">
+      <Link
+        to={props.link}
+        activeClassName="activeMenu"
+        activeStyle={{ color: "green" }}
+      >
+        {props.label}
+      </Link>
+    </div>
+  );
 };
 export default MenuCardComponent;
