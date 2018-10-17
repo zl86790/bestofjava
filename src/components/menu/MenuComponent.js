59f661ebf5c749efcfd2dde96c510419ba0d9f2e
pactera-admindev-lizhe <zhe.li4@pactera.com>
2018-10-17 16:41:53 +0800

    update menu style

--- src/components/menu/MenuComponent.js
+++ src/components/menu/MenuComponent.js
@@ -6,15 +6,60 @@ const MenuComponent = props => {
   return (
     <div className="bojLeftMenu">
       <nav>
-        <MenuCard label="Home" link="/" />
-        <MenuCard label="Activiti" link="/topic/activiti" />
-        <MenuCard label="Lucene" link="/topic/lucene" />
-        <MenuCard label="Mybatis" link="/topic/mybatis" />
-        <MenuCard label="Maven" link="/topic/maven" />
-        <MenuCard label="Rabbitmq" link="/topic/rabbitmq" />
-        <MenuCard label="Redis" link="/topic/redis" />
-        <MenuCard label="Rest" link="/topic/rest" />
-        <MenuCard label="Spring" link="/topic/spring" />
+        <MenuCard
+          label="Home"
+          link="/"
+          d="M16 9l-3-3V2h-2v2L8 1 0 9h2l1 5c0 .55.45 1 1 1h8c.55 0 1-.45 1-1l1-5h2zm-4 5H9v-4H7v4H4L2.81 7.69 8 2.5l5.19 5.19L12 14z"
+          menuStyle="menuCard oneStyle"
+        />
+        <MenuCard
+          label="Activiti"
+          link="/topic/activiti"
+          d="M0 14l6-6-6-6v12z"
+          menuStyle="menuCard twoStyle"
+        />
+        <MenuCard
+          label="Lucene"
+          link="/topic/lucene"
+          d="M0 14l6-6-6-6v12z"
+          menuStyle="menuCard twoStyle"
+        />
+        <MenuCard
+          label="Mybatis"
+          link="/topic/mybatis"
+          d="M0 14l6-6-6-6v12z"
+          menuStyle="menuCard twoStyle"
+        />
+        <MenuCard
+          label="Maven"
+          link="/topic/maven"
+          d="M0 14l6-6-6-6v12z"
+          menuStyle="menuCard twoStyle"
+        />
+        <MenuCard
+          label="Rabbitmq"
+          link="/topic/rabbitmq"
+          d="M0 14l6-6-6-6v12z"
+          menuStyle="menuCard twoStyle"
+        />
+        <MenuCard
+          label="Redis"
+          link="/topic/redis"
+          d="M0 14l6-6-6-6v12z"
+          menuStyle="menuCard twoStyle"
+        />
+        <MenuCard
+          label="Rest"
+          link="/topic/rest"
+          d="M0 14l6-6-6-6v12z"
+          menuStyle="menuCard twoStyle"
+        />
+        <MenuCard
+          label="Spring"
+          link="/topic/spring"
+          d="M0 14l6-6-6-6v12z"
+          menuStyle="menuCard twoStyle"
+        />
       </nav>
     </div>
   );
--- src/components/menu/menuCard/MenuCardComponent.js
+++ src/components/menu/menuCard/MenuCardComponent.js
@@ -4,14 +4,31 @@ import { Link } from "react-router-dom";
 
 const MenuCardComponent = props => {
   return (
-    <div className="menuCard">
-      <Link
-        to={props.link}
-        activeClassName="activeMenu"
-        activeStyle={{ color: "green" }}
-      >
-        {props.label}
-      </Link>
+    <div className={props.menuStyle}>
+      <div className="row no-gutter">
+        <div className="col-md-1" style={{ marginLeft: 20 }}>
+          <svg
+            width="20"
+            height="20"
+            class="octicon octicon-home"
+            viewBox="0 0 16 16"
+            version="1.1"
+            aria-hidden="true"
+            fill="rgb(250, 158, 89)"
+          >
+            <path fillRule="evenodd" d={props.d} />
+          </svg>
+        </div>
+        <div className="col-md-3" style={{ marginTop: 3 }}>
+          <Link
+            to={props.link}
+            activeClassName="activeMenu"
+            activeStyle={{ color: "green" }}
+          >
+            {props.label}
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
